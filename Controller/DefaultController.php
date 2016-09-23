a1f0354d93dc1b36a37146522234923e1387d866
David Teruel <d_teruel@hotmail.com>
2016-09-23 17:32:00 +0200

    Initial commit

---
+++ Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace MrJeff\CommonBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('CommonBundle:Default:index.html.twig');
+    }
+}
